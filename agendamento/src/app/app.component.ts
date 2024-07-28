78255801f05ee69ee274e6686480f71e1058126c
Lais Portela <lais3722@gmail.com>
2024-07-28 18:19:40 -0300

    finalizando segundo agendamento pelo máximo número de tarefas

--- agendamento/src/app/app.component.ts
+++ agendamento/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormArray, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
-import { Job, Schedule } from './models/job.model';
+import { Job, JobSchedule, Schedule, ScheduleLateness } from './models/job.model';
 
 @Component({
   selector: 'app-root',
@@ -9,82 +9,165 @@ import { Job, Schedule } from './models/job.model';
 })
 export class AppComponent {
   title = 'agendamento';
-  jobCounter: number = 0;
 
+  scheduleLatenessForm!: FormGroup;
+  latenessMax: number = 0;
+  startTime!: number;
+  scheduleLateness: ScheduleLateness[] = [];
+
+
+  scheduleMaxForm!: FormGroup;
   schedule: Schedule[] = [];
 
-  myForm!: FormGroup;
-  latenessMax: number = 0;
 
-  startTime!: number;
+  jobCounterLateness: number = 0;
+  jobCounterMax: number = 0;
 
-  JOB: Job[] = [
-    new Job("Job 1", 3, 13),
-    new Job("Job 2", 2, 9),
-    new Job("Job 3", 1, 15),
-    new Job("Job 4", 4, 14),
-    new Job("Job 5", 3, 9),
-    new Job("Job 6", 2, 6),
-    new Job("Job 7", 4, 8),
-    new Job("Job 8", 1, 6)
-];
+  type!: number;
 
-
+  
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
-    this.myForm = this.fb.group({
-      jobs: this.fb.array([this.createItem()]),
+    this.scheduleLatenessForm = this.fb.group({
+      jobs: this.fb.array([this.createItemLateness()]),
       startTime: [null, [Validators.required, this.timeValidator]]
+    });
+
+    this.scheduleMaxForm = this.fb.group({
+      jobs: this.fb.array([this.createItemMax()]),
     });
   }
 
-  get jobs(): FormArray {
-    return this.myForm.get('jobs') as FormArray;
+  choiceSchedule(id: number){
+    if(id == 0)
+      this.type = 0;
+    else this.type = 1;
   }
 
-  createItem(): FormGroup {
-    this.jobCounter +=1;
+  get jobsLateness(): FormArray {
+    return this.scheduleLatenessForm.get('jobs') as FormArray;
+  }
+  get jobsMax(): FormArray {
+    return this.scheduleMaxForm.get('jobs') as FormArray;
+  }
+
+
+  createItemLateness(): FormGroup {
+    this.jobCounterLateness +=1;
     return this.fb.group({
-      name: [`Job ${this.jobCounter}`, Validators.required],
+      name: [`Job ${this.jobCounterLateness}`, Validators.required],
       duration: [null, Validators.required],
       deadline: [null, Validators.required]
     });
   }
 
+  createItemMax(): FormGroup {
+    this.jobCounterMax +=1;
+    return this.fb.group({
+      name: [`Job ${this.jobCounterMax}`, Validators.required],
+      start: [null, [Validators.required, this.timeValidator]],
+      finish: [null, [Validators.required, this.timeValidator]]
+    });
+  }
+
   addItem() {
-    this.jobs.push(this.createItem());
+    if(this.type == 1)
+      this.jobsLateness.push(this.createItemLateness());
+    else this.jobsMax.push(this.createItemMax());
   }
 
   removeItem(index: number) {
-    this.jobs.removeAt(index);
+    if(this.type == 1 )
+      this.jobsLateness.removeAt(index);
+    else this.jobsMax.removeAt(index)
   }
 
-  onSubmit() {
-    console.log(this.myForm.value);
-    this.convertStartTime();
-    this.schedule = this.minimizeLateness(this.jobs.value);
-    console.log(this.minimizeLateness(this.jobs.value))
 
-    
+
+
+
+
+
+
+
+  timeValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+    if (value && !/^\d{2}:\d{2}$/.test(value)) {
+      return { invalidTime: 'Formato inválido, o formato correto é HH:MM' };
+    }
+    return null;
   }
 
-  convertMinutesToHoursDay(horas: number): string {
-    const [sh, sm]= this.myForm.get('startTime')?.value.split(':').map(Number);
-    const total = sh * 60 + sm + horas *60;
+ 
 
-    const days = Math.floor(total / 1440); // 1440 minutos em um dia
-    const minTotal = total % 1440;
 
-    const h = Math.floor(minTotal / 60);
-    const m = minTotal % 60;
+  ///////////////////////////////// Max Schedule///////////////////////////////////////
 
-    const formattedHours = h.toString().padStart(2, '0');
-    const formattedMinutes = m.toString().padStart(2, '0');
-    return (days > 0 ? `Dia ${days+1} às ` : 'Dia 1 às ') +`${formattedHours}:${formattedMinutes}`;
+
+  resultMax(){
+    console.log(this.scheduleMaxForm.value);
+    this.schedule = this.scheduleIntervals(this.jobsMax.value);
+    console.log("hh", this.schedule)
+
   }
 
-  convertMinutesToHours(horas: number){
+  convertHour(value: string): number {
+    const [h, m] = value.split(':').map(Number);
+    return h * 60 + m;
+  }
+  
+  scheduleIntervals(jobs: JobSchedule[]): Schedule[] {
+    jobs.sort((a, b) => this.convertHour(a.finish) - this.convertHour(b.finish));
+  
+    const list: Schedule[] = [];
+    let lastFinishTime = 0;
+  
+    for (const job of jobs) {
+      const jobStart = this.convertHour(job.start);
+      const jobFinish = this.convertHour(job.finish);
+  
+      if (jobStart >= lastFinishTime) {
+        list.push({
+          start: job.start,
+          finish: job.finish,
+          name: job.name
+        });
+        lastFinishTime = jobFinish;
+      }
+    }
+  
+    return list;
+  }
+
+  exportToCSVSchedule(): void {
+    const csvData = this.schedule.map((item, index) => ({
+      '#': index + 1,
+      'Nome da Tarefa': item.name,
+      'Horário Início': item.start,
+      'Horário de Fim': item.finish,
+    }));
+
+    const header = ['#', 'Nome da Tarefa', 'Horário Início', 'Horário de Fim'];
+    const rows = csvData.map(item => Object.values(item).join(','));
+
+    const csvContent = [header.join(','), ...rows].join('\n');
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'tarefa.csv');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+
+
+  ////////////////////////////// Lateness ////////////////////////////////////////////
+
+  convertHourToHHMM(horas: number){
 
     const h = Math.floor(horas*60 / 60);
     const m = horas*60 % 60;
@@ -95,13 +178,49 @@ export class AppComponent {
 
   }
 
+
+  convertDay(horas: number): string {
+
+    let total = 0;
+    if(this.type == 1){
+      const [sh, sm]= this.scheduleLatenessForm.get('startTime')?.value.split(':').map(Number);
+      total = sh * 60 + sm + horas *60;
+    }else{
+      total =  horas *60;
+    }
+
+
+    const days = Math.floor(total / 1440);
+    const minTotal = total % 1440;
+
+    const h = Math.floor(minTotal / 60);
+    const m = minTotal % 60;
+
+    const formattedHours = h.toString().padStart(2, '0');
+    const formattedMinutes = m.toString().padStart(2, '0');
+    return (days > 0 ? `Dia ${days+1} às ` : 'Dia 1 às ') +`${formattedHours}:${formattedMinutes}`;
+  }
+
+  convertStartTime(){
+    const [h, m] = this.scheduleLatenessForm.get('startTime')?.value.split(':').map(Number);
+    this.startTime = h *60 + m;
+  }
+
+  resultLateness() {
+    console.log(this.scheduleLatenessForm.value);
+    this.convertStartTime();
+    this.scheduleLateness = this.minimizeLateness(this.jobsLateness.value);
+    console.log(this.minimizeLateness(this.jobsLateness.value))
+  }
+
+
   minimizeLateness(jobs: Job[]){
 
    
     jobs.sort((a, b) => Number(a.deadline) - (b.deadline));
 
     let time: number = 0;
-    const list: Schedule[] = [];
+    const list: ScheduleLateness[] = [];
 
     this.latenessMax = 0;
     
@@ -112,7 +231,6 @@ export class AppComponent {
       const lateness = Math.max(0, finish - Number(job.deadline));
       console.log(lateness, finish,job.deadline)
       if(lateness > this.latenessMax){
-        console.log("enttou", lateness)
         this.latenessMax = lateness;
       }
       list.push({name: job.name, start: start, finish: finish, lateness: lateness})
@@ -122,16 +240,28 @@ export class AppComponent {
     return list;
   }
 
-  timeValidator(control: AbstractControl): ValidationErrors | null {
-    const value = control.value;
-    if (value && !/^\d{2}:\d{2}$/.test(value)) {
-      return { invalidTime: 'Formato inválido, o formato correto é HH:MM' };
-    }
-    return null;
-  }
+  exportToCSVLateness(): void {
+    const csvData = this.scheduleLateness.map((item, index) => ({
+      'Nº': index + 1,
+      'Nome da Tarefa': item.name,
+      'Horário Início': this.convertDay(item.start),
+      'Horário de Fim': this.convertDay(item.finish),
+      'Atraso': this.convertHourToHHMM(item.lateness),
 
-  convertStartTime(){
-    const [h, m] = this.myForm.get('startTime')?.value.split(':').map(Number);
-    this.startTime = h *60 + m;
+    }));
+
+    const header = ['Nº', 'Nome da Tarefa', 'Horário Início', 'Horário de Fim', 'Atraso'];
+    const rows = csvData.map(item => Object.values(item).join(','));
+
+    const csvContent = [header.join(','), ...rows].join('\n');
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'tarefa.csv');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 }
--- agendamento/src/app/models/job.model.ts
+++ agendamento/src/app/models/job.model.ts
@@ -12,12 +12,34 @@ export class Job{
   
 }
 
+export class JobSchedule{
+    name: string;
+    start: string;
+    finish: string;
 
-export class Schedule{
+    constructor(name: string, start: string, finish: string) {
+        this.name = name;
+        this.start = start;
+        this.finish = finish;
+      
+    }
+  
+}
+
+
+
+export class ScheduleLateness{
     name!: string;
     start!: number;
     finish!: number;
     lateness!: number;  // Usando Moment.js para representar a data/hora
 
     
+}
+
+
+export class Schedule{
+    name!: string;
+    start!: string;
+    finish!: string;
 }
